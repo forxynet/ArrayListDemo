29eae7c11d2fa65161c07f0f94e154b30595d02a
forxynet <forxynet@gmail.com>
2020-06-12 22:07:36 +0300

    C# 7.0 Array List Demo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListDemo {
+    class Program {
+        static void Main(string[] args) {
+            ArrayList Numbers = new ArrayList(1);
+            Numbers.Add(100);
+            Numbers.Add(200);
+            Numbers.Add(300);
+            Numbers.Add(400);
+            foreach (int Number in Numbers) {
+                Console.WriteLine(Number + "  ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
